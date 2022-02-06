4e3dcff9e3f0e4dc12b07c02b03e96eb656a91d8
luffy981 <jhonsmith981@hotmail.com>
2022-02-06 17:54:43 -0500

    Flask initialization

--- init_flask.sh
+++ init_flask.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
-export FLASK_APP=$2
-export FLASK_DEBUG=1
-flask run
+if [[ $2 ]]; then
+  export FLASK_APP=$2
+  export FLASK_DEBUG=1
+  flask run
+elif [[ !$2 ]]; then
+  
+  echo "Usage: ./init_flask.sh [filename]"
+  echo "Try again please..."
+fi 
+
+
+
+
